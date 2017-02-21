20a90885c63cc074fcfdd37cd2399d6ff4bd573a
mogudy <mogudy@gmail.com>
2017-02-21 11:38:22 +0800

    更新了connector，增加了db
    Signed-off-by: mogudy <mogudy@gmail.com>

--- nbms/connector/connector.go
+++ nbms/connector/connector.go
@@ -9,19 +9,22 @@ import (
 	"errors"
 	"github.com/mogudy/golib/nbms/servant"
 	"github.com/mogudy/golib/nbms/messenger"
+	"database/sql"
 )
 
 type(
 	ConsulService interface {
+		GetDbConnection()(*sql.DB)
 		RegisterMessageHandler(string, func([]byte)[]byte)(error)
 		RegisterHttpHandler(string, bool, func([]byte)([]byte))
 		StartServer(bool)
 	}
 	service struct {
-		agent servant.ConsulAgent
-		config *NbConfig
+		agent     servant.ConsulAgent
+		config    *NbConfig
 		messenger messenger.Connector
-		started bool
+		started   bool
+		db        *sql.DB
 	}
 )
 
@@ -42,8 +45,9 @@ type(
 		Tags []string
 	}
 	NbServerInfo struct {
-		Server string
-		Port uint32
+		Address  string
+		Port     uint32
+		Name     string
 		Username string `default:"guest"`
 		Password string `default:"guest"`
 	}
@@ -61,22 +65,34 @@ func CreateService(filepath string) (ConsulService, error){
 	if err != nil{ return nil, err }
 	cfile.MustLoad(config)
 
+	// setup db connection & validate it
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		config.Database.Username, config.Database.Password,
+		config.Database.Address, config.Database.Port, config.Database.Name))
+	if err != nil {return nil, err}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {return nil, err}
+
 	// Register Service & Health check
 	agent, err := servant.NewConsulClient(fmt.Sprintf("%s:%d",config.Service.Server,config.Service.Port))
 	if err != nil{ return nil, err }
 	agent.Register(fmt.Sprintf("%s-%s",config.Service.Name,time.Now().Format("2006010215")), config.Service.Tags, 80)
 	defer agent.DeRegister()
 
-	return &service{agent:agent,config:config,started:false},nil
+	return &service{agent: agent,config: config,started: false, db: db},nil
 }
 
+func (s *service)GetDbConnection()*sql.DB{
+	return s.db
+}
 func (s *service)RegisterMessageHandler(topic string, callback func([]byte)([]byte)) (error){
 	// create a mq agent if not exist
-	if s.config.Amqp.Port == 0 || s.config.Amqp.Server == "" {
-		return errors.New("Amqp Server Configuration is missing.")
+	if s.config.Amqp.Port == 0 || s.config.Amqp.Address == "" {
+		return errors.New("Amqp Address Configuration is missing.")
 	}
 	if s.messenger == nil {
-		msger, err := messenger.CreateMessenger(s.config.Service.Name, s.config.Amqp.Username, s.config.Amqp.Password, s.config.Amqp.Server, s.config.Amqp.Port)
+		msger, err := messenger.CreateMessenger(s.config.Service.Name, s.config.Amqp.Username, s.config.Amqp.Password, s.config.Amqp.Address, s.config.Amqp.Port)
 		if err!=nil{
 			return err
 		}
