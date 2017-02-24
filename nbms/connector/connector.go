273b8dcc535236960ece91b427870d07aa511124
mogudy <mogudy@gmail.com>
2017-02-25 00:15:48 +0800

    增加了原生mysql连接
    Signed-off-by: mogudy <mogudy@gmail.com>

--- nbms/connector/connector.go
+++ nbms/connector/connector.go
@@ -10,6 +10,7 @@ import (
 	"github.com/mogudy/golib/nbms/servant"
 	"github.com/mogudy/golib/nbms/messenger"
 	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type(
@@ -71,13 +72,13 @@ func CreateService(filepath string) (ConsulService, error){
 	cfile.MustLoad(config)
 
 	// setup db connection & validate it
-	//db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
-	//	config.Database.Username, config.Database.Password,
-	//	config.Database.Address, config.Database.Port, config.Database.Name))
-	//if err != nil {return nil, err}
-	////defer db.Close()
-	//err = db.Ping()
-	//if err != nil {return nil, err}
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		config.Database.Username, config.Database.Password,
+		config.Database.Address, config.Database.Port, config.Database.Name))
+	if err != nil {return nil, err}
+	//defer db.Close()
+	err = db.Ping()
+	if err != nil {return nil, err}
 
 	// Register Service & Health check
 	agent, err := servant.NewConsulClient(fmt.Sprintf("%s:%d",config.Service.Server,config.Service.Port))
